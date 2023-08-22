58dc9e10543b7286f26251938a47446a43c56158
Avinash <palakurthiavinash07@gmail.com>
2023-08-22 12:09:22 +0530

    pagination done

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Pagination() {
+  let [pageChange, setPageChange] = useState(1);
+
+  const handlerPreviousPage = () => {
+    console.log("previousPage");
+    if (pageChange > 1) {
+      setPageChange(pageChange - 1);
+    }
+  };
+
+  const handlerNextPage = () => {
+    console.log("nextPage");
+
+    setPageChange(pageChange + 1);
+  };
   return (
     <div className="flex mt-6 justify-center text-xl font-semibold my-8 ">
-      <div className="border-2 border-blue-400 p-2 border-r-0 rounded-l-xl">
+      <div
+        className="border-2 border-blue-400 p-2 border-r-0 rounded-l-xl"
+        onClick={handlerPreviousPage}
+      >
         Previous
       </div>
       <div className="border-2 border-blue-400 p-2 border-r-0 bg-blue-500">
-        1
+        {pageChange}
       </div>
-      <div className="border-2 border-blue-400 p-2 rounded-r-xl">Next</div>
+      <div
+        className="border-2 border-blue-400 p-2 rounded-r-xl"
+        onClick={handlerNextPage}
+      >
+        Next
+      </div>
     </div>
   );
 }
